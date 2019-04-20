1d786cb12a9d17f9d9d89e66bcfca93c6e7091bc
lordkleiton <m_s_r@outlook.com>
2019-04-19 21:34:29 -0300

    adding db data

---
+++ db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE aaa;
+
+USE aaa;
+
+CREATE TABLE produtos(
+    id INT NOT NULL AUTO_INCREMENT, 
+    nomes VARCHAR(255) NOT NULL, 
+    valor DECIMAL(10, 2) NOT NULL, 
+    descricao VARCHAR(1000) NOT NULL, 
+    img VARCHAR(255) NOT NULL, 
+    PRIMARY KEY (id)
+);
