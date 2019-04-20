3ada58b19c63f153fcd8f7283f94e32ca6aa0d4e
lordkleiton <m_s_r@outlook.com>
2019-04-20 18:52:20 -0300

    addesd content

---
+++ components/delete.php
@@ -0,0 +1,6 @@
+<h1 class="center">O seguinte produto foi deletado:</h1>
+<div class="conteudo">
+    <h1 class="center"><?php echo $linha["nomes"] ?></h1>
+    <p>Preço:<br>R$ <?php echo $linha["valor"] ?></p>
+    <p>Descrição:<br><?php echo $linha["descricao"] ?></p>
+</div>
--- view/deleted.php
+++ view/deleted.php
@@ -1,5 +1,18 @@
-<?php 
-    session_start();
 
-    var_dump($_SESSION['deleted']);
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../components/head.html' ?>
+    <title>Produto deletado</title>
+</head>
+<body>
+    <?php 
+        session_start();
+
+        foreach($_SESSION['deleted'] as $linha){
+            include '../components/delete.php';
+        }
+    ?>
+</body>
+</html>
