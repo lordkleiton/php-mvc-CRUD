a5da8c10bcf7dad9e99806aa6a5d5127b599835c
lordkleiton <m_s_r@outlook.com>
2019-04-19 21:35:06 -0300

    adding all bd operations to the class

---
+++ model/bd.php
@@ -0,0 +1,99 @@
+<?php 
+    include "../config.php";
+
+    class BD{
+        private $pdo;
+
+        public function __construct(){
+            $this->conecta();
+        }
+
+        private function conecta(){
+            $inst = new PDO('mysql:host='.HOST.';dbname='.DB, USER, PASSWORD);
+            $this->setPDO($inst);
+        }
+
+        public function getPDO(){
+            return $this->$pdo;
+        }
+
+        public function setPDO($arg){
+            $this->$pdo = $arg;
+        }
+
+        public function getAll(){
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QSELECTALL);
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        }
+
+        public function getLike($nome){
+            $nome = $nome.'%';
+
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QSELECT);
+            $stmt->execute(array(
+                ':nome' => $nome
+            ));
+
+            return $stmt->fetchAll();
+        }
+
+        public function getById($id){
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QSELECTBYID);
+            $stmt->execute(array(
+                ':id' => $id
+            ));
+
+            return $stmt->fetchAll();
+        }
+
+        public function getLastInserted(){
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QSELECTLASTINSERTED);
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        }
+
+        public function delete($id){
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QDELETE);
+            $stmt->execute(array(
+                ':id' => $id
+            ));
+
+            return true;
+        }
+
+        public function insert($nome, $valor, $desc, $img){
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QINSERT);
+            $stmt->execute(array(
+                ':nomes' => $nome,
+                ':valor' => $valor,
+                ':descricao' => $desc,
+                ':img' => $img
+            ));
+
+            return true;
+        }
+
+        public function update($id, $nome, $valor, $desc, $img){
+            $pdo = $this->getPDO();
+            $stmt = $pdo->prepare(QUPDATE);
+            $stmt->execute(array(
+                ':id' => $id,
+                ':nomes' => $nome,
+                ':valor' => $valor,
+                ':descricao' => $desc,
+                ':img' => $img,
+            ));
+
+            return true;
+        }
+    }
+?>
