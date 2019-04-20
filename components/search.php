50981a077c241fd44caf4f40f6af6e30eab8ff3f
lordkleiton <m_s_r@outlook.com>
2019-04-20 19:00:11 -0300

    creating new file

---
+++ components/search.php
@@ -0,0 +1,7 @@
+<div class="conteudo">
+    <h1 class="center"><?php echo $linha["nomes"] ?></h1>
+    <p>Código do produto:<br><?php echo $linha["id"] ?></p>
+    <p>Preço:<br>R$ <?php echo $linha["valor"] ?></p>
+    <p>Descrição do produto:<br><?php echo $linha["descricao"] ?></p>
+    <img <?php echo "src=\"".IMGPATH.$linha["img"]."\"" ?> <?php echo "alt=\"".$linha["descricao"]."\"" ?>>
+</div>
