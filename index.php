f4c26b9f8d5412d01f7e390c8140842832635008
lordkleiton <m_s_r@outlook.com>
2019-04-19 22:07:15 -0300

    testing the classes

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    include "config.php";
+    include "model/product.php";
+
+    $t = new Product();
+
+    //var_dump($t->getAll());
+    //var_dump($t->getLike('b'));
+    //var_dump($t->getById(21));
+    //var_dump($t->getLastInserted());
+    //var_dump($t->delete(17));
+    //var_dump($t->insert('owo', 125, 'owo', 'owo.uwu'));
+    //var_dump($t->update(22, 'uwo', 420, 'uwu', 'uwu.owo'));
+
+    //var_dump($t->getAll());
+    //$t->setNome('oie');
+    //echo $t->getNome();
+    //$t->setValor('oiew');
+    //echo $t->getValor();
+    //var_dump($t->getAll());
+?>
