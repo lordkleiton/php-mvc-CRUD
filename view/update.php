a6e13fa0241230f560af7c7e4537979c077151ac
lordkleiton <m_s_r@outlook.com>
2019-04-20 21:35:50 -0300

    separating into modules

--- view/update.php
+++ view/update.php
@@ -15,22 +15,9 @@
 </head>
 <body>
     <a href="../index.php"><div class="btnVoltar"><</div></a>
-    <div class="conteudo">
-        <form action="../controller/update.php" method="POST" enctype="multipart/form-data">
-            <label for="nome">Insira o nome do produto: </label>
-            <input type="text" name="name" id="nome" placeholder="Ex: Casa" <?php echo "value=\"".$aux['nomes']."\"" ?> required><br>
-            <label for="preco">Insira o preço do produto: </label>
-            <input type="number" name="price" id="preco" placeholder="Ex: 8.50" <?php echo "value=\"".$aux['valor']."\"" ?> required><br>
-            <label for="descricao">Insira a descrição do produto: </label>
-            <input type="text" name="desc" id="descricao" placeholder="Ex: Casa bonita" <?php echo "value=\"".$aux['descricao']."\"" ?> required><br>
-            <label for="campoImg">Selecione uma imagem para o produto (PNG, JPG ou JPEG): </label>
-            <input type="file" name="img" id="campoImg"><br>
-            <label for="check">Manter imagem atual?</label>
-            <input type="checkbox" name="keepImg" id="check"><br>
-            <img <?php echo "src=\"".IMGPATH.$aux["img"]."\"" ?> <?php echo "alt=\"".IMGPATH.$aux["descricao"]."\"" ?>><br>
-            <input type="submit" value="Enviar" name="submit">
-        </form>
-    </div>
+    <?php 
+        include '../components/update.php';
+    ?>
 </body>
 </html>
 
